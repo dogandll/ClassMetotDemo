aa6869e0a307db4174cbc353b8580d444befd871
Doğan DELLAL <dogandellal@gmail.com>
2021-08-22 05:37:33 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Tebrikler, müşteri eklendi :" + musteri.Ad + ' ' + musteri.Soyad);
+            }
+            
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("{0} isimli müşteri silindi !", musteri.Ad);
+        }
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine(item.Ad);
+            }
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Doğan";
+            musteri1.Soyad = "Dellal";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "İnci Sare";
+            musteri2.Soyad = "Dellal";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "İmge Deniz";
+            musteri3.Soyad = "Dellal";
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("--------------- Müşteri Ekleme ---------------");
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            musteriManager.Ekle(musteriler);
+
+            Console.WriteLine("--------------- Müşteri Listeleme ---------------");
+            
+            musteriManager.Listele(musteriler);
+
+            Console.WriteLine("--------------- Müşteri Silme ! ---------------");
+            musteriManager.Sil(musteri3);
+
+            Console.ReadKey();
+
+        }
+    }
+}
